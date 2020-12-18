06bf255e022a25459a5ab3c80248eb5200d48855
Jason Weirather <jason-weirather@users.noreply.github.com>
2020-12-18 07:37:31 -0500

    add helper functions for reading standard format projects and samples

--- pythologist_reader/formats/inform/__init__.py
+++ pythologist_reader/formats/inform/__init__.py
@@ -6,6 +6,7 @@ import os
 from tempfile import NamedTemporaryFile
 def read_standard_format_project(path,
                              annotation_strategy,
+                             channel_abbreviations={},
                              project_name='myproject',
                              temp_dir=None,
                              custom_mask_name=None,
@@ -41,15 +42,18 @@ def read_standard_format_project(path,
         if annotation_strategy=='GIMP_CUSTOM':
             cpi = CellProjectInFormCustomMask(temp_path.name,mode='w')
             for sample in samples:
-                _do_GIMP_CUSTOM(cpi,os.path.join(path,sample),export,sample,custom_mask_name,other_mask_name,verbose)
+                csi = _do_GIMP_CUSTOM(os.path.join(path,sample),export,sample,custom_mask_name,other_mask_name,channel_abbreviations,verbose)
+                cpi.append_sample(csi)
         elif annotation_strategy=='GIMP_TSI':
             cpi = CellProjectInFormLineArea(temp_path.name,mode='w')
             for sample in samples:
-                _do_GIMP_TSI(cpi,os.path.join(path,sample),export,sample,line_pixel_steps,verbose)
+                csi = _do_GIMP_TSI(os.path.join(path,sample),export,sample,line_pixel_steps,channel_abbreviations,verbose)
+                cpi.append_sample(csi)
         else:
             cpi = CellProjectInForm(temp_path.name,mode='w')
             for sample in samples:
-                _do_GIMP_OTHER(cpi,os.path.join(path,sample),export,sample,verbose)
+                csi = _do_GIMP_OTHER(os.path.join(path,sample),export,sample,channel_abbreviations,verbose)
+                cpi.append_sample(csi)
         outputs[export] = cpi
         cpi.project_name = export
         cpi.microns_per_pixel = microns_per_pixel
@@ -57,10 +61,11 @@ def read_standard_format_project(path,
 
     return outputs
 
-def _do_GIMP_CUSTOM(cpi,base_path,export,sample_name,custom_mask_name,other_mask_name,verbose):
+def _do_GIMP_CUSTOM(base_path,export,sample_name,custom_mask_name,other_mask_name,channel_abbreviations,verbose):
         csi = CellSampleInFormCustomMask()
         csi.read_path(
                       path=os.path.join(base_path,'INFORM_ANALYSIS',export),
+                      channel_abbreviations=channel_abbreviations,
                       sample_name=sample_name,
                       verbose = verbose,
                       require = False,
@@ -69,11 +74,12 @@ def _do_GIMP_CUSTOM(cpi,base_path,export,sample_name,custom_mask_name,other_mask
                       other_mask_name = other_mask_name,
                       alternate_annotation_path = os.path.join(base_path,'ANNOTATIONS')
                      )
-        cpi.append_sample(csi)
-def _do_GIMP_TSI(cpi,base_path,export,sample_name,line_pixel_steps,verbose):
+        return csi
+def _do_GIMP_TSI(base_path,export,sample_name,line_pixel_steps,channel_abbreviations,verbose):
         csi = CellSampleInFormLineArea()
         csi.read_path(
                       path=os.path.join(base_path,'INFORM_ANALYSIS',export),
+                      channel_abbreviations=channel_abbreviations,
                       sample_name=sample_name,
                       verbose = verbose,
                       require = False,
@@ -81,22 +87,24 @@ def _do_GIMP_TSI(cpi,base_path,export,sample_name,line_pixel_steps,verbose):
                       steps=line_pixel_steps,
                       alternate_annotation_path = os.path.join(base_path,'ANNOTATIONS')
                      )
-        cpi.append_sample(csi)
-def _do_GIMP_OTHER(cpi,base_path,export,sample_name,verbose):
+        return csi
+def _do_GIMP_OTHER(base_path,export,sample_name,channel_abbreviations,verbose):
         csi = CellSampleInForm()
         csi.read_path(
                       path=os.path.join(base_path,'INFORM_ANALYSIS',export),
+                      channel_abbreviations=channel_abbreviations,
                       sample_name=sample_name,
                       verbose = verbose,
                       require = False,
                       require_score = False,
                       alternate_annotation_path = os.path.join(base_path,'ANNOTATIONS')
                      )
-        cpi.append_sample(csi)
+        return csi
 
 
-def read_standard_format_sample(path,
+def read_standard_format_sample_to_project(path,
                              annotation_strategy,
+                             channel_abbreviations={},
 	                         sample=None,
                              project_name='myproject',
                              temp_dir=None,
@@ -107,7 +115,7 @@ def read_standard_format_sample(path,
                              line_pixel_steps=76
                              ):
     """
-    Read a sample structed in a standardized format.
+    Read a sample structed in a standardized format into a project.  Some advanced CellDataFrame functions may require a project.
 
     Return a dictinary of CellProjects keyed to the export names
     """
@@ -124,13 +132,16 @@ def read_standard_format_sample(path,
         temp_path = NamedTemporaryFile(prefix='project-',suffix='.h5',dir=temp_dir,delete=False)
         if annotation_strategy=='GIMP_CUSTOM':
             cpi = CellProjectInFormCustomMask(temp_path.name,mode='w')
-            _do_GIMP_CUSTOM(cpi,path,export,sample,custom_mask_name,other_mask_name,verbose)
+            csi = _do_GIMP_CUSTOM(path,export,sample,custom_mask_name,other_mask_name,channel_abbreviations,verbose)
+            cpi.append_sample(csi)
         elif annotation_strategy=='GIMP_TSI':
             cpi = CellProjectInFormLineArea(temp_path.name,mode='w')
-            _do_GIMP_TSI(cpi,path,export,sample,line_pixel_steps,verbose)
+            csi = _do_GIMP_TSI(path,export,sample,line_pixel_steps,channel_abbreviations,verbose)
+            cpi.append_sample(csi)
         else:
             cpi = CellProjectInForm(temp_path.name,mode='w')
-            _do_GIMP_OTHER(cpi,path,export,sample,verbose)
+            csi = _do_GIMP_OTHER(path,export,sample,channel_abbreviations,verbose)
+            cpi.append_sample(csi)
         outputs[export] = cpi
         cpi.project_name = export
         cpi.microns_per_pixel = microns_per_pixel
@@ -138,7 +149,41 @@ def read_standard_format_sample(path,
 
     return outputs
 
+def read_standard_format_sample(path,
+                             annotation_strategy,
+                             channel_abbreviations={},
+	                         sample=None,
+                             project_name='myproject',
+                             custom_mask_name=None,
+                             other_mask_name=None,
+                             microns_per_pixel=0.496,
+                             verbose=False,
+                             line_pixel_steps=76
+                             ):
+    """
+    Read a sample structed in a standardized format.
 
+    Return a dictinary of CellSample keyed to the export names
+    """
+    _check_annotation_strategy(annotation_strategy)
+    exports = [x for x in os.listdir(os.path.join(path,'INFORM_ANALYSIS')) if x[0]!='.']
+    outputs = {}
+
+    # If we dont supply a sample name assume its the directory being referenced as per standard layout
+    if sample is None:
+    	_temp, sample = os.path.split(path)
+
+    # prepare the exports
+    for export in exports:
+        if annotation_strategy=='GIMP_CUSTOM':
+            csi = _do_GIMP_CUSTOM(path,export,sample,custom_mask_name,other_mask_name,channel_abbreviations,verbose)
+        elif annotation_strategy=='GIMP_TSI':
+            csi = _do_GIMP_TSI(path,export,sample,line_pixel_steps,channel_abbreviations,verbose)
+        else:
+            csi = _do_GIMP_OTHER(path,export,sample,channel_abbreviations,verbose)
+        outputs[export] = csi
+
+    return outputs
 
 def _check_annotation_strategy(annotation_strategy):
     if annotation_strategy not in ['GIMP_TSI','GIMP_CUSTOM','INFORM_ANALYSIS','NO_ANNOTATION']:
